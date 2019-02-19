4ac875f9e0363db17e5ed92b7799b097d11f4789
Temirlan <t.smagulov@luxystech.com>
2019-02-19 13:03:32 +0600

    add filter

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -3,15 +3,29 @@ import React, { Component } from 'react';
 import './item-status-filter.css';
 
 export default class ItemStatusFilter extends Component {
+
+  constructor() {
+    super();
+
+    console.log(this.props);
+  }
+
+
   render() {
     return (
       <div className="btn-group">
         <button type="button"
-          className="btn btn-info">All</button>
+          className="btn btn-info"
+          onClick={ (e) => { this.props.onFilter('all', e.target) } }
+          >All</button>
         <button type="button"
-          className="btn btn-outline-secondary">Active</button>
+          className="btn btn-outline-secondary"
+          onClick={ (e) => { this.props.onFilter('active', e.target) } }
+          >Active</button>
         <button type="button"
-          className="btn btn-outline-secondary">Done</button>
+          className="btn btn-outline-secondary"
+          onClick={ (e) => { this.props.onFilter('dones', e.target) } }
+          >Done</button>
       </div>
     );
   }
